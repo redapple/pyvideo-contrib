71fc187f99ce44d5d0bbe45da6941297c3444161
Paul Tremberth <paul.tremberth@gmail.com>
2015-12-10 02:09:46 +0100

    Fix markdown conversion using html2text pipeline

--- pyconfr2015/pyconfr2015/pyconfr2015/pipelines.py
+++ pyconfr2015/pyconfr2015/pyconfr2015/pipelines.py
@@ -5,7 +5,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import html2text
 
-class Pyconfr2015Pipeline(object):
+class Html2TextPipeline(object):
     def process_item(self, item, spider):
+        for f in ('description_md', 'abstract_md'):
+            val = item.get(f)
+            if val:
+                item[f] = html2text.html2text(val)
         return item
