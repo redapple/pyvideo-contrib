804fc47150a7351263f5dc803b5184eff76800e3
Paul Tremberth <paul.tremberth@gmail.com>
2015-12-12 02:03:05 +0100

    Add JSON files in pyvideo/steve format

--- pyconfr2015/ffprobe_duration.sh
+++
@@ -1,5 +0,0 @@
-cat pyconfr2015/videos.json | jq -r '.[] | .videos | .[] | .src' | grep --color=never ".ogv" | while read -r line ; do
-    echo "Processing $line"
-    ffprobe "$line" 2>&1 | grep -i duration:
-    echo
-done
